292893704ea4bdd924d684232f483a0ae5bdccde
Ricco Førgaard <ricco@fiskeben.dk>
2025-05-21 10:42:40 +0200

    Bundle vision

--- src/lib/server/vision.ts
+++ src/lib/server/vision.ts
@@ -1,7 +1,9 @@
 import { ImageAnnotatorClient } from '@google-cloud/vision';
+import { GOOGLE_CLOUD_CREDENTIALS } from '$env/static/private';
 
-// Initialize the client with v1p4beta1 API version
+// Initialize the client with credentials from environment
 export const visionClient = new ImageAnnotatorClient({
+  credentials: JSON.parse(GOOGLE_CLOUD_CREDENTIALS),
   apiEndpoint: 'vision.googleapis.com',
   apiVersion: 'v1p4beta1'
 }); 
