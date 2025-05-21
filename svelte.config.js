9ed626e745e669ce070eb9ef1e792b9abe8a1979
Ricco Førgaard <ricco@fiskeben.dk>
2025-05-21 10:49:10 +0200

    Configures Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
 		adapter: adapter({
-			// see below for options that can be set here
+			runtime: 'nodejs18',
+			regions: ['fra1'], // optional: specify deployment region
+			edge: false // disable edge runtime since we're using Google Cloud Vision
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
